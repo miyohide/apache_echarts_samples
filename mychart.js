8348611cf14981e3c0380ba16c38dc36dd0f5cf2
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-18 08:12:00 +0900

    create first simple app

---
+++ mychart.js
@@ -0,0 +1,24 @@
+var myChart = echarts.init(document.getElementById('main'));
+
+var option = {
+    title: {
+        text: 'Hello ECharts'
+    },
+    tooltip: {},
+    legend: {
+        data: ['sales']
+    },
+    xAxis: {
+        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+    },
+    yAxis: {},
+    series: [
+        {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }
+    ]
+};
+
+myChart.setOption(option);
