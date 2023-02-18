a6bf1fbeeec97d3ab602c7bd7d4713a88581d948
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-18 14:16:42 +0900

    run prettier

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import * as echarts from 'echarts';
+import * as echarts from "echarts";
 
-const index = echarts.init(document.getElementById('main'));
+const index = echarts.init(document.getElementById("main"));
 
 const option = {
-    title: {
-        text: 'Hello ECharts'
+  title: {
+    text: "Hello ECharts",
+  },
+  tooltip: {},
+  legend: {
+    data: ["sales"],
+  },
+  xAxis: {
+    data: ["Shirts", "Cardigans", "Chiffons", "Pants", "Heels", "Socks"],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: "sales",
+      type: "bar",
+      data: [5, 20, 36, 10, 10, 20],
     },
-    tooltip: {},
-    legend: {
-        data: ['sales']
-    },
-    xAxis: {
-        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
-    },
-    yAxis: {},
-    series: [
-        {
-            name: 'sales',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }
-    ]
+  ],
 };
 
 index.setOption(option);
