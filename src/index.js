7a45295dbf04ccc0567dc1b08491b0a4e9d1c2c2
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-18 20:23:55 +0900

    use axios

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 import * as echarts from "echarts";
+import axios from "axios";
 
 const index = echarts.init(document.getElementById("main"));
 
-const option = {
-  title: {
-    text: "Hello ECharts",
-  },
-  tooltip: {},
-  legend: {
-    data: ["sales"],
-  },
-  xAxis: {
-    data: ["Shirts", "Cardigans", "Chiffons", "Pants", "Heels", "Socks"],
-  },
-  yAxis: {},
-  series: [
-    {
-      name: "sales",
-      type: "bar",
-      data: [5, 20, 36, 10, 10, 20],
-    },
-  ],
-};
+axios
+  .get("http://localhost:8080/data.json")
+  .then(function (response) {
+    const option = {
+      title: {
+        text: "Hello ECharts",
+      },
+      tooltip: {},
+      legend: {
+        data: ["sales"],
+      },
+      xAxis: {
+        data: response.data.categories,
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "sales",
+          type: "bar",
+          data: response.data.values,
+        },
+      ],
+    };
 
-index.setOption(option);
+    index.setOption(option);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
